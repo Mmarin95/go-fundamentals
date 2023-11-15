769dc0e990a9f3154ebfad616075d11d83c7bfde
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2023-11-15 14:42:48 +0100

    Add concurrency to get multiple currency rates

--- cmasters/main.go
+++ cmasters/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"miqgo.com/go/cmasters/api"
 )
 
 func main() {
 
-	rate, error := api.GetRate("BTC")
+	currencies := []string{"BTC", "ETH", "BHC"}
+	var wg sync.WaitGroup
+
+	for _, currency := range currencies {
+		wg.Add(1)
+		go func(curr string) {
+			getCurrencyFor(curr)
+			wg.Done()
+		}(currency)
+	}
+	wg.Wait()
+}
+
+func getCurrencyFor(currency string) {
+	rate, error := api.GetRate(currency)
 	if error != nil {
 		panic(error)
 	}
 
-	fmt.Printf("1 BTC = %f USD\n", rate.Price)
-
+	fmt.Printf("1 %s = %f USD\n", currency, rate.Price)
 }
